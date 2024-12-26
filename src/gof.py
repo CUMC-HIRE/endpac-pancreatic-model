7500af972813619cf06e06191cd014dfd8367bd1
sophiewagner7 <sophie.wagn@gmail.com>
2024-12-26 10:46:02 -0500

    feat: added sojourn time calculation

--- src/gof.py
+++ src/gof.py
@@ -1,40 +1,21 @@
 import numpy as np
 import configs as c
 import common_functions as func
-from csaps import csaps
 
-smoothL=csaps(c.ages_1y, c.seer_inc_1y['LocalU'], smooth=0.01)(c.ages_1y)
-smoothR=csaps(c.ages_1y, c.seer_inc_1y['RegionalU'], smooth=0.01)(c.ages_1y)
-smoothD=csaps(c.ages_1y, c.seer_inc_1y['DistantU'], smooth=0.01)(c.ages_1y)
-
-# SEER 5y incidence
-def objective_avg_risk_5y(log):
-    inc, _, _, _= log
-    score = 0
-
-    loc_5y, reg_5y, dis_5y = [func.get_5y_means(inc[stage, :]) for stage in [4, 5, 6]]
-
-    score += np.square(loc_5y - c.seer_inc["LocalU"]).sum()
-    score += np.square(reg_5y - c.seer_inc["RegionalU"]).sum()
-    score += np.square(dis_5y - c.seer_inc["DistantU"]).sum()
-
-    return score
-
-# SEER 1y incidence
-def objective_avg_risk_1y(log):
+# Average risk object function
+def objective(log):
     inc, _, _, _= log
     score = 0
     
-
-    # Calculate weighted scores
-    score += np.square(inc[4, :] - smoothL).sum()  # Local stage
-    score += np.square(inc[5, :] - smoothR).sum()  # Regional stage
-    score += np.square(inc[6, :] - smoothD).sum()  # Distant stage
+    # Yearly incidence penalty
+    score += np.square(inc[4, :] - c.seer_inc['LocalU']).sum()  # Local stage
+    score += np.square(inc[5, :] - c.seer_inc["RegionalU"]).sum()  # Regional stage
+    score += np.square(inc[6, :] - c.seer_inc["DistantU"]).sum()  # Distant stage
 
     return score
 
 # NOD objective function
-def objective(log):
+def objective_nod(log):
     inc, _, _, _= log
     score = 0
 
@@ -58,3 +39,31 @@ def objective_post_nod(log):
     score += np.square(loc - data['LocalU']).sum()
     score += np.square(reg - data['RegionalU']).sum()
     score += np.square(dis - data["DistantU"]).sum()
+    
+    
+def cancer_progression(tmat):
+    """
+    Calculate cancer progression rate using MFPT formula.
+    """
+    p_12 = tmat[:, 1, 2] # loc to reg
+    p_23 = tmat[:, 2, 3] # reg to dis
+    p_11 = tmat[:, 1, 1] # stay loc
+    p_22 = tmat[:, 2, 2] # stay reg
+    p_33 = tmat[:, 3, 3] # stay dis
+    
+    cp = (1 + p_12 * (1 + p_23 * (1 / (1 - p_33))) * (1 / (1 - p_22))) * (1 / (1 - p_11))
+    
+    return cp
+    
+def sojourn_time(tmat):
+    """
+    Calculate mean sojourn time.
+    """
+    
+    sojourn_times = np.zeros((3,len(c.ages_1y)))
+    for i in np.arange(1,4,1):
+        p_stay = tmat[:, i, i]
+        sojourn_times[i-1] = 1 / (1 - p_stay)
+    avg_sojourn_time = np.mean(sojourn_times, axis=0)
+    
+    return avg_sojourn_time
